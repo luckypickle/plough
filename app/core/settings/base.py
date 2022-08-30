7bf51a95c74a1eccb50a880ce29ca163f76e6315
mutalisk999 <tangjian999999999@gmail.com>
2022-08-30 14:06:07 +0800

    issue fixed

--- app/core/settings/base.py
+++ app/core/settings/base.py
@@ -10,7 +10,7 @@ class AppEnvTypes(Enum):
 
 
 class BaseAppSettings(BaseSettings):
-    app_env: str = AppEnvTypes.prod
+    app_env: AppEnvTypes = AppEnvTypes.prod
 
-    class Config:
-        env_file = ".env"
+    # class Config:
+    #     env_file = ".env"
