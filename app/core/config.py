636fe7afe23fe7f094ea127d7f4f9f44e6aeefbb
mutalisk999 <tangjian999999999@gmail.com>
2022-08-31 17:32:00 +0800

    fix

--- app/core/config.py
+++ app/core/config.py
@@ -7,16 +7,16 @@ from app.core.settings.development import DevAppSettings
 from app.core.settings.production import ProdAppSettings
 from app.core.settings.test import TestAppSettings
 
-environments: Dict[str, Type[AppSettings]] = {
-    AppEnvTypes.dev: DevAppSettings,
-    AppEnvTypes.prod: ProdAppSettings,
-    AppEnvTypes.test: TestAppSettings,
+environments: Dict[AppEnvTypes, Type[AppSettings]] = {
+    AppEnvTypes(AppEnvTypes.dev): DevAppSettings,
+    AppEnvTypes(AppEnvTypes.prod): ProdAppSettings,
+    AppEnvTypes(AppEnvTypes.test): TestAppSettings,
 }
 
 
 @lru_cache
 def get_app_settings() -> AppSettings:
-    app_env = BaseAppSettings().app_env
+    app_env: AppEnvTypes = BaseAppSettings().app_env
     print(app_env)
     config = environments[app_env]
     return config()
