4697580cbc81ffd46b544dc6c544a0db9b7043ca
tedmosby <530623363@qq.com>
2022-09-13 15:18:28 +0800

    add invite order info for backend

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -8,7 +8,7 @@ from .product import Product, ProductCreate, ProductUpdate, ProductForOrder
 from .comment import Comment, CommentCreate, CommentUpdate,CommentQuery,CommentListQuery,CommentFullData
 from .version import Version, VersionCreate, VersionUpdate
 from .history import History, HistoryCreate, HistoryUpdate
-from .invite import Invite, InviteCreate, InviteUpdate, InviteForInfo, InvitedDetailUsers,InvitedUserDetail
+from .invite import Invite, InviteCreate, InviteUpdate, InviteForInfo, InvitedDetailUsers,InvitedUserDetail,InviteOrder,InviteOrderInfo
 from .reward import Reward, RewardCreate, RewardUpdate, RewardDetail,RewardDetailInfos
 from .withdraw import Withdraw, WithdrawCreate, WithdrawUpdate
 from .bill import BillQuery
