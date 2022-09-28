55a644703de968a8ed6d5db32857876b7d3c87e2
sunnypickle <443237831@qq.com>
2022-09-28 17:41:39 +0800

    fix master bugs

--- app/bazi/bazi.py
+++ app/bazi/bazi.py
@@ -336,7 +336,6 @@ def cal_wuxing_color(year,month,day_,hour,minute,day_delta:int=0):
         day=Zhi[dGZ.dz], time=Zhi[hGZ.dz])
 
     todaydate = datetime.date.today() + datetime.timedelta(days=day_delta)
-    print(todaydate)
     today = sxtwl.fromSolar(
         todaydate.year, todaydate.month, todaydate.day)
     tyGZ = today.getYearGZ()
@@ -415,9 +414,9 @@ def cal_wuxing_color(year,month,day_,hour,minute,day_delta:int=0):
     day_map = ["初一", "初二", "初三", "初四", "初五", "初六", "初七", "初八", "初九", "初十", "十一", "十二", "十三", "十四", "十五", "十六", "十七",
                "十八", "十九", "二十",
                "廿一", "廿二", "廿三", "廿四", "廿五", "廿六", "廿七", "廿八", "廿九", "三十"]
-    Lleap = "闰" if day.isLunarLeap() else ""
-    lunar_str = "{}年  {}{}月{}".format(Gan[tyGZ.tg] + Zhi[tyGZ.dz], Lleap, month_map[day.getLunarMonth() - 1],
-                                    day_map[day.getLunarDay() - 1])
+    Lleap = "闰" if today.isLunarLeap() else ""
+    lunar_str = "{}年  {}{}月{}".format(Gan[tyGZ.tg] + Zhi[tyGZ.dz], Lleap, month_map[today.getLunarMonth() - 1],
+                                    day_map[today.getLunarDay() - 1])
     jieqi_map = ["冬至", "小寒", "大寒", "立春", "雨水", "惊蛰", "春分", "清明", "谷雨", "立夏", "小满", "芒种", "夏至", "小暑", "大暑", "立秋", "处暑",
                  "白露", "秋分", "寒露", "霜降", "立冬", "小雪", "大雪"]
     jieqi = ""
