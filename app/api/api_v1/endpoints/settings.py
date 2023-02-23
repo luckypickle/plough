a6b458083a48120056b4633ecff9539856f3f7f1
yajunli1130 <yajunli1130@163.com>
2023-02-23 14:54:58 +0800

    update divinationsettings

--- app/api/api_v1/endpoints/settings.py
+++ app/api/api_v1/endpoints/settings.py
@@ -14,42 +14,44 @@ router = APIRouter()
 def create_or_update_divination_settings(
         *,
         db: Session = Depends(deps.get_db),
-        entry_type: int = 0,
-        show_type: int = 0,
-        analysis_isClose: bool = False,
-        taimingshen_isClose: bool = False,
-        xingyun_isClose: bool = False,
-        liuri_isClose: bool = False,
+        entry_type: int = None,
+        show_type: int = None,
+        analysis_isClose: bool = None,
+        taimingshen_isClose: bool = None,
+        xingyun_isClose: bool = None,
+        liuri_isClose: bool = None,
         current_user: models.User = Depends(deps.get_current_user),
 ) -> Any:
-    userId,masterId = None,None
+    divination_settings_in = {}
+    if entry_type is not None:
+        divination_settings_in["entry_type"]=entry_type
+    if show_type is not None:
+        divination_settings_in["show_type"]=show_type
+    if analysis_isClose is not None:
+        divination_settings_in["analysis_isClose"]=analysis_isClose
+    if taimingshen_isClose is not None:
+        divination_settings_in["taimingshen_isClose"]=taimingshen_isClose
+    if xingyun_isClose is not None:
+        divination_settings_in["xingyun_isClose"]=xingyun_isClose
+    if liuri_isClose is not None:
+        divination_settings_in["liuri_isClose"]=liuri_isClose
     divination_settings = None
     if isinstance(current_user, models.User):
-        userId=current_user.id
-        divination_settings = crud.divination_settings.get_by_user_id(db=db, user_id=userId)
+        divination_settings_in["user_id"]=current_user.id
+        divination_settings = crud.divination_settings.get_by_user_id(db=db, user_id=current_user.id)
     elif isinstance(current_user,models.Master):
-        masterId=current_user.id
+        divination_settings_in["master_id"]=current_user.id
         divination_settings = crud.divination_settings.get_by_master_id(db=db, master_id=current_user.id)
     else:
         raise HTTPException(
             status_code=404,
             detail="Insufficient permissions",
         )   
-    divination_settings_in = schemas.DivinationSettings(
-        user_id=userId,
-        master_id=masterId,        
-        entry_type=entry_type,
-        show_type=show_type,
-        analysis_isClose=analysis_isClose,
-        taimingshen_isClose=taimingshen_isClose,
-        xingyun_isClose=xingyun_isClose,
-        liuri_isClose=liuri_isClose
-    )
     if divination_settings is None:
-        crud.divination_settings.create_divination_settings(db, divination_settings=divination_settings_in)
+        divination_settings = crud.divination_settings.create(db, obj_in=divination_settings_in)
     else:
-        crud.divination_settings.update(db, db_obj=divination_settings, obj_in=divination_settings_in)
-    return divination_settings_in
+        divination_settings = crud.divination_settings.update(db, db_obj=divination_settings, obj_in=divination_settings_in)
+    return divination_settings
 
 @router.get("/divination_settings", response_model=schemas.DivinationSettingsQuery)
 def get_divination_settings(
