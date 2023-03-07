c56a1d53f650c4558bcc221729e9612fddbd7e66
yajunli1130 <yajunli1130@163.com>
2023-03-07 11:11:23 +0800

    add HistoryCombine

--- app/models/history_combine.py
+++ app/models/history_combine.py
@@ -10,11 +10,13 @@ class HistoryCombine(Base):
     birthday1 = Column(String)
     location1 = Column(String, index=True)
     divination1 = Column(String)
+    isNorth1 = Column(Boolean, default=True, comment="是否北半球")
     name2 = Column(String, index=True)
     sex2 = Column(Integer, index=True)
     birthday2 = Column(String)
     location2 = Column(String, index=True)
     divination2 = Column(String)
+    isNorth2 = Column(Boolean, default=True, comment="是否北半球")
     owner_id = Column(Integer, ForeignKey("user.id"))
     create_time = Column(DateTime, server_default=func.now(), index=True, comment="创建时间")
 
--- app/schemas/history_combine.py
+++ app/schemas/history_combine.py
@@ -11,11 +11,13 @@ class HistoryCombineBase(BaseModel):
     sex1: Optional[int] = 1
     location1: Optional[str] = ''
     divination1: Optional[str] = ''
+    isNorth1:Optional[bool]=True
     name2: Optional[str] = None
     birthday2: Optional[str] = None
     sex2: Optional[int] = 1
     location2: Optional[str] = ''
     divination2: Optional[str] = ''
+    isNorth2:Optional[bool]=True
 
 # Properties to receive via API on creation
 class HistoryCombineCreate(HistoryCombineBase):
